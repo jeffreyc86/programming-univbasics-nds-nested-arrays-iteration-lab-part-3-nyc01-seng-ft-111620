8cb12bf75ee434ea3693fdfba1ef8fcef26157b2
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-01 22:59:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,9 +3,14 @@ def join_nested_strings(src)
   row_index = 0 
   while row_index < src.count do
     element_index = 0 
-    whil
-  new_string = new_array.join()
+    while element_index < src[row_index].count do
+      if src[row_index][element_index] = String
+        new_array.push (src[row_index][element_index])
+      end
+    element_index += 1 
+    end
+  new_string = new_array.join(" ")
   row_index += 1
   end
- 
+ new_string
 end
